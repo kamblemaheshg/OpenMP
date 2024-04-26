502d50af86651ef149ca10026c809b7f8920cedf
mahi <themaheshkamble@gmail.com>
2024-04-26 19:35:38 +0530

    parallel for

---
+++ parallel_for.c
@@ -0,0 +1,23 @@
+/* parallel_for.c */
+
+#include <omp.h>
+#include <stdio.h>
+
+int main()
+{
+
+    int a = 10;
+
+#pragma omp parallel for num_threads(1000) lastprivate(a)
+    for (int i = 0; i < 10; i++)
+    {
+        a++;
+        printf("Thread of pid = %d , i = %d \n", omp_get_thread_num(), a);
+    }
+    printf("A outside parallel region = %d \n", a);
+
+    return 0;
+}
+
+
+
