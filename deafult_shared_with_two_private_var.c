e92c3c5731f16b1dfc04401155b491b8e179ce4d
mahi <themaheshkamble@gmail.com>
2024-04-26 23:10:25 +0530

    error checking

--- deafult_shared_with_two_private_var.c
+++ deafult_shared_with_two_private_var.c
@@ -1,27 +1,33 @@
+/* deafult_shared_with_two_private_var.c */
+
 #include <stdio.h>
 #include <omp.h>
 
 int main()
 {
+    int x;
 
-    int x = 10;
-    int y = 20;  
+   
+    printf("Out side Value of x= %d\n", x);
 
-#pragma omp parallel /*default(none)*/   private(x) shared(y) default(shared) firstprivate(x)
+    int y = 20;
+
+#pragma omp parallel default(shared) private(x, y)
     {
-
-        int x = 10;
-        int y = 20;
         /* x is private to each thread */
         /* y is shared among all thread */
 
         printf("Value of x= %d\n", x);
+        printf("Value of x= %d\n", y);
 
         x = omp_get_thread_num();
+        y += x;
         printf("Thread %d : x = %d, y = %d\n", omp_get_thread_num(), x, y);
 
         printf("\n");
     }
+
+    printf("outside parallel region x = %d, y = %d\n", x, y);
 
     return 0;
 }
