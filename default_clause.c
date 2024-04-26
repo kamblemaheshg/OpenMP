70b17846d1ae4b2e2f4c53bec149d17ecbcd2e66
mahi <themaheshkamble@gmail.com>
2024-04-26 21:20:33 +0530

    last

---
+++ default_clause.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    int x = 10;
+    int y = 20;
+  
+    #pragma omp parallel default(shared) private(x)
+    {
+        /* x is private to each thread */
+        /* y is shared among all thread */
+
+        printf("Value of x= %d\n",x);
+        
+        x=omp_get_thread_num();
+        printf("Thread %d : x = %d, y = %d\n",omp_get_thread_num(),x,y);
+
+        printf("\n");
+    }
+    
+
+    return 0;
+}
