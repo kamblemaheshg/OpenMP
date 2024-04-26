9ab7c9f270b1e84167e06cb85852d31a554eee4f
mahi <themaheshkamble@gmail.com>
2024-04-26 22:12:08 +0530

    default none

---
+++ deafult_none_clause.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main()
+{
+
+    int x = 10;
+    int y = 20;
+
+#pragma omp parallel /*default(none)*/   private(x) shared(y) default(shared) firstprivate(x)
+    {
+
+        int x = 10;
+        int y = 20;
+        /* x is private to each thread */
+        /* y is shared among all thread */
+
+        printf("Value of x= %d\n", x);
+
+        x = omp_get_thread_num();
+        printf("Thread %d : x = %d, y = %d\n", omp_get_thread_num(), x, y);
+
+        printf("\n");
+    }
+
+    return 0;
+}
